4a99ab899aadf2ca6d0ff4eae5ddc0a42d3e6eef
Lorenzo raspberrypizero <lorereds@gmail.com>
2019-06-23 19:02:00 +0000

    Automated Daily Commit

--- shell_run.sh
+++ shell_run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /home/pi/Tokyo/roadtotokyo.github.io
-#../venv/bin/python Data/DataGathering/main.py
-#../venv/bin/python jinjify.py
+git pull
+../venv/bin/python Data/DataGathering/main.py
+../venv/bin/python jinjify.py
 git add .
 git commit -m 'Automated Daily Commit'
 git push
